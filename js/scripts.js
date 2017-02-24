96538ed0932669d68f9b2e9a418c08a71f8e56cf
CoderKnot <coder.knot@gmail.com>
2017-02-24 15:15:10 -0800

    add simple input error checking

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,8 @@
 // Business Logic
 var pingPong = function(number) {
-
   var numbers = [];
 
   for(var i = 1; i <= number; i++) {
-
     var replaceNumber = "";
 
     if(i % 15 === 0) {
@@ -19,30 +17,35 @@ var pingPong = function(number) {
     } else {
       numbers.push(i);
     }
-
   }
-
-  console.log(numbers);
   return numbers;
-
 }
 
 // User Interface Logic
 $(document).ready(function() {
   $("form#ping-pong").submit(function(event) {
-
     event.preventDefault();
+
     $('#numbersList').empty();
+    $("#numbers-showing").hide();
+    $("#numbers-error").hide();
+    $("#numbers-hidden").show();
 
     var number = parseInt($("#number").val());
-    var numbers = pingPong(number);
 
-    for(var i = 0; i < numbers.length; i++) {
-      $("#numbersList").append("<li>" + numbers[i] + "</li>");
+    if(jQuery.type(number === "number") && ((number !== number) === false)) {
+      var numbers = pingPong(number);
+
+      for(var i = 0; i < numbers.length; i++) {
+        $("#numbersList").append("<li>" + numbers[i] + "</li>");
+      }
+
+      $("#numbers-hidden").hide();
+      $("#numbers-showing").show();
+    } else {
+      $("#numbers-hidden").hide();
+      $("#numbers-error").show();
     }
 
-    $("#output").show();
-
   });
-
 });
