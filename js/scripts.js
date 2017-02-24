b8ea0f745c2b044ec10326328ec3174ae94ee4b9
CoderKnot <coder.knot@gmail.com>
2017-02-24 11:44:58 -0800

    add basic framework to scripts.js for input testing

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+// Business Logic
+var pingPong = function(number) {
+
+  var numbers = [];
+
+  for(var i = 1; i <= number; i++) {
+    numbers.push(i);
+  }
+
+  console.log(numbers);
+  return numbers;
+}
+
+// User Interface Logic
+$(document).ready(function() {
+  $("form#ping-pong").submit(function(event) {
+
+    event.preventDefault();
+    $('#numbersList').empty();
+
+    var number = parseInt($("#number").val());
+    var numbers = pingPong(number);
+
+    for(var i = 0; i < numbers.length; i++) {
+      $("#numbersList").append("<li>" + numbers[i] + "</li>");
+    }
+
+    $("#output").show();
+
+  });
+
+});
